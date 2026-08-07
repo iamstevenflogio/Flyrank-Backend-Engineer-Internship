b0d754fe5aa9126da3dfd390f04af6aa46079e53
Steven Iefel Flogio <69651154+iamstevenflogio@users.noreply.github.com>
2026-08-07 23:00:12 +0800

    Stage 0: hello server

--- CRUD-API/index.js
+++ CRUD-API/index.js
@@ -1,32 +1,11 @@
-const express = require('express');  
+const express = require('express');
 const app = express();
-const PORT = 8080;
+const port = 3000;
 
-app.use( express.json() )
-
-app.listen(
-    PORT,
-    () => console.log(`it's alive on http://localhost:${PORT}`)
-)    
-
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: '=H=',
-        size: 'large'
-    })
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
-
-    if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' })
-    }
-
-    res.send({
-        tshirt: `=H= with your ${logo} and ID ${id}`,
-
-    });
-
+app.listen(port, () => {
+  console.log(`it's alive on http://localhost:${port}`);
 });
