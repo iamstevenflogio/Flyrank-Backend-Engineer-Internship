eeefa68b79682eb8254792fe7bc10e8b66b25dea
Steven Iefel Flogio <69651154+iamstevenflogio@users.noreply.github.com>
2026-08-07 23:31:10 +0800

    Stage 2: read endpoints with 404

--- CRUD-API/index.js
+++ CRUD-API/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// In-memory data: this resets when we restart server
+const tasks = [
+    {id: 1, title: "Learn Express basics", done: false},
+    {id: 2, title: "Create a Task API", done: true},
+    {id: 3, title: "Test endpoints in Insomnia", done: true}
+]
+
 app.listen(port, () => {
   console.log(`it's alive on http://localhost:${port}`);
 });
@@ -19,5 +26,21 @@ app.get('/health', (req, res) => {
         "status": "ok"
     })
 });
+
+app.get('/tasks', (req, res) => {
+    res.send({tasks})
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find((task) => task.id === id);
+
+    if (!task) {
+        return res.status(404).json(`error: Task ${id} not found`)
+    }
+    res.send(task)
+})
+
+
 
 
