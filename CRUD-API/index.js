3761a10dc9b6d3973474f281bc4d6847f86e9286
Steven Iefel Flogio <69651154+iamstevenflogio@users.noreply.github.com>
2026-08-09 02:44:57 +0800

    Stage 3: create with validation

--- CRUD-API/index.js
+++ CRUD-API/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 // In-memory data: this resets when we restart server
 const tasks = [
     {id: 1, title: "Learn Express basics", done: false},
     {id: 2, title: "Create a Task API", done: true},
-    {id: 3, title: "Test endpoints in Insomnia", done: true}
+    {id: 3, title: "Test endpoints in Insomnia", done: true},
 ]
+
+
 
 app.listen(port, () => {
   console.log(`it's alive on http://localhost:${port}`);
@@ -17,7 +21,7 @@ app.get('/', (req, res) => {
   res.send({
     "name": "Task API",
     "version": "1.0",
-    "endpoints": "[/tasks]"
+    endpoints: "[/tasks]"
   });
 });
 
@@ -28,7 +32,7 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/tasks', (req, res) => {
-    res.send({tasks})
+    res.json(tasks)
 })
 
 app.get('/tasks/:id', (req, res) => {
@@ -36,11 +40,37 @@ app.get('/tasks/:id', (req, res) => {
     const task = tasks.find((task) => task.id === id);
 
     if (!task) {
-        return res.status(404).json(`error: Task ${id} not found`)
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
     }
     res.send(task)
 })
 
+app.post('/tasks', (req, res) => {
+    const { title } = req.body;
+
+    if (!title || !title.trim()) {
+        return res.status(400).json({
+            error: "title is required and cannot be empty"
+        });
+    }
+
+    const nextId = 
+        tasks.length > 0
+            ? Math.max(...tasks.map(task => task.id)) + 1
+            : 1;
+
+    const newTask = { 
+        id: nextId,
+        title: title.trim(),
+        done: false
+    };
+
+    tasks.push(newTask)
+
+    res.status(201).json(newTask)
+});
 
 
 
