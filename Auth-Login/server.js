c0aebb7e8200b8347c7bff41d653f2537d2c55af
Steven Iefel Flogio <69651154+iamstevenflogio@users.noreply.github.com>
2026-08-19 08:56:52 +0800

    Stage 1 and login routes working

--- Auth-Login/server.js
+++ Auth-Login/server.js
@@ -20,6 +20,43 @@ app.get('/', (req, res) => {
     });
 });
 
+// Let the front gates open! POST routes lets users register their keys and come back with them.
+// POST /auth/signup
+app.post("/auth/signup", async (req,res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" });
+    }
+
+    const { data, error } = await supabase.auth.signUp({ email, password, });
+
+    if (error) {
+        return res.status(400).json({ error: error.message })
+    }
+
+    return res.status(201).json(data.user);
+});
+
+// POST /auth/login
+app.post("/auth/login", async (req,res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" })
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword  ({ email, password, });
+
+    if (error) {
+        return res.status(401).json({ error: "Invalid login credentials" });
+    }
+        return res.status(200).json({
+            access_token: data.session.access_token, 
+            refresh_token: data.session.refresh_token,
+        });
+});
+    
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
